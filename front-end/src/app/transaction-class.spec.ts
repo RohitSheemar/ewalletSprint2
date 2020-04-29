fc77fa531ac1c251e691499a730e229430eaeb4f
RohitSheemar <40451508+RohitSheemar@users.noreply.github.com>
2020-04-29 22:02:27 +0530

    commit

--- front-end/src/app/transaction-class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TransactionClass } from './transaction-class';
-
-describe('TransactionClass', () => {
-  it('should create an instance', () => {
-    expect(new TransactionClass()).toBeTruthy();
-  });
-});
